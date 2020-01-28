c0aa8cb5e359387eb5be75a8a02cac2bb6f3e26f
Yusuf Celep <yusufcelep8@gmail.com>
2020-01-28 11:23:16 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,8 +1,7 @@
 first_number = 1
 second_number = 1
 
-def sum (first_number, second_number)
-  sum = first_number + second_number
+sum = first_number + second_number
 
 def difference (first_number, second_number)
   first_number - second_number

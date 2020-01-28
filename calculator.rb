c461bbce80f2e209fd22723f51a1dd6658072acc
Yusuf Celep <yusufcelep8@gmail.com>
2020-01-28 11:19:45 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,18 @@
-# Add your variables here
+first_number = 1
+second_number = 0
+
+def sum (first_number, second_number)
+  first_number + second_number
+end
+
+def difference
+
+end
+
+def product
+  
+end
+
+def quotient
+  
+
